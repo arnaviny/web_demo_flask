a070a09666da369cc26f702f4675a1be1114c8b6
arnaviny <109102974+arnaviny@users.noreply.github.com>
2023-10-02 23:50:31 +0300

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config')
+    app.config.from_object('config.Config')  # הוספתי את הסיומת .Config
 
     db.init_app(app)
 
